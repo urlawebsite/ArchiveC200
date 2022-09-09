12fd35991eadaea343b94fe265db18de27be3e22
uzrivera <uzrivera@iu.edu>
2022-09-09 14:31:34 -0400

    Lab1

---
+++ Laboratory/Lab1/Lab1.py
@@ -0,0 +1,7 @@
+def computeShippingPrice(height, width, depth, weight):
+    return 5 * height * width * depth + 0.5 * weight
+
+
+if __name__ == "__main__":
+    x = computeShippingPrice(10, 10, 10, 10)
+    print("Compute shippingPrice:", x)
