8b3f7cdb7f7a1b19aaf55f802a1e7d4a1256807a
uzrivera <uzrivera@iu.edu>
2022-09-16 14:11:28 -0400

    Lab2

---
+++ Laboratory/Lab2/Lab2.py
@@ -0,0 +1,91 @@
+
+def speedometer(pos1, pos2, time):
+    """
+    Problem Description
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Calculate the speed of a moving car.
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Input:
+        pos1 - the initial position of the car
+        pos2 - the final position of the car
+        time - the recorded time the car spent between them
+    Returns: The speed of the car.
+    """
+    pass
+
+
+def warping(speed):
+    """
+    Problem Description
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    In the wonderful world of Star Trek, they have a way to travel faster than light. Below is a table of the speed representation. 
+    The table is from "The Star Trek Encyclopedia: A Reference Guide to the Future. Updated and Expanded Edition" by Michael Okuda 
+    and Denise Okuda.
+    Speed                   |    Number of times speed of light (Number represents lower bound)
+    Sublight(Warp Factor 0) |    0
+    Warp Factor 1           |    1
+    Warp Factor 2           |    10
+    Warp Factor 3           |    39
+    Warp Factor 4           |    102
+    Warp Factor 5           |    214
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Input: A float or number
+    Returns: A number representing the warp factor
+    """
+    pass
+
+
+def catch_speeders(pos1, pos2, time):
+    """
+    Problem Description
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    You are a member of the Star Fleet space-travel law enforcement in the world of Star Trek.
+    You have caught a ship -- the USS Enterprise -- speeding in the past, and you wish to catch
+    them again.  The legal warp speed is 3 -- anything above this and you need to notify your
+    superiors.  Write a function which signals whenever warp speed detected is above 3.
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Input:
+        pos1 - the initial position of the spaceship
+        pos2 - the final position of the spaceship
+        time - the recorded time
+    Returns: A signal indicating that the warp factor exceeds 3.
+    """
+    pass
+
+
+def myTestString(func, params):
+    """
+    Do not modify
+    """
+    return func.__name__ + "" + str(params) + " produces " + str(func(*params))
+
+
+#Examples to see the difference between "print" and "return"
+def function_that_prints():
+    print("I printed")
+
+
+def function_that_returns():
+    return "I returned"
+
+
+# For more information about the line below, refer to the link: https://docs.python.org/3/library/__main__.html
+#   You can read more about it but it is to help when handling multiple files
+#   Will be dicussed at a later point
+if __name__ == "__main__":
+    print("Final Test Code\n")
+    print("Testing speedometer()")
+    for x in [(0.0,4.0,2.0), (-1,5,40), (0,0,1.0), (6,3,33)]:
+        print(myTestString(speedometer, x))
+    print("Testing warping()")
+    for x in [(0,), (41.0,),(10.2,), (1111,)]:
+        print(myTestString(warping, x))
+    print("Testing catch_speeders()")
+    for x in [(145,150,1.0), (125,150,1.0), (25,150,1.0), (5,150,1.0)]:
+        print(myTestString(catch_speeders, x))
+    
+    f1 = function_that_prints()
+    f2 = function_that_returns()
+    print("Now let us see what the values of f1 and f2 are")
+    print(f1)
+    print(f2)
