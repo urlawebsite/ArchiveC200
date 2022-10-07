35d5ce7d37f8175f3a210972a31b2eba07430d14
uzrivera <uzrivera@iu.edu>
2022-10-07 15:05:07 -0400

    Fixed Syntax.txt

---
+++ Laboratory/Lab5/Notes5.py
@@ -0,0 +1,27 @@
+# i = 0
+# while True:
+#     if i == 4:
+#         break
+#     print("I am in the Loop")
+#     i += 1
+
+# for i in range(4):
+#     print("I am in the loop")
+
+# lst = ['a', 'b', 'c', 'd', 'f']
+# for item in lst:
+#     print(item)
+#     if item == 'd':
+#         break
+
+# While and Bounded Loop didn't work
+
+
+y = (lambda x: x**2)(2)
+print(y)
+
+w = (lambda x, y, z: x**2+2*y-z)(5, 6, 7)
+print(w)
+
+r = 10 % 3
+print(r)
