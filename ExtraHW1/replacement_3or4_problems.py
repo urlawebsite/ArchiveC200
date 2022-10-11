2f2c15d040e8441547d3df975f2771dd342bd5e9
uzrivera <uzrivera@iu.edu>
2022-10-11 14:49:56 -0400

    Added .DS to git

--- ExtraHW1/replacement_3or4_problems.py
+++ ExtraHW1/replacement_3or4_problems.py
@@ -8,42 +8,64 @@ import math
 # input 0,1,2,...
 # return factorial
 def factorial(n):
-    pass
+    a = math.factorial(n)
+    return a
 
 # problem 2
 # input two lists (containing numbers) of same length
-# return a list of the greater number at each position of the input lists 
+# return a list of the greater number at each position of the input lists
+
+
 def gl(lst1, lst2):
+    nlst = []
+    for i in range(len(lst1)):
+        if lst1[i] > lst2[i]:
+            nlst.append(lst1)
+        else:
+            nlst.append(lst2)
+    return nlst
+
+
+# problem 3
+# INPUT argument n and number of terms
+# RETURN approximation of e(n) for num_terms.
+def my_e(n, num_terms):
     pass
-
-
-
-#problem 3
-#INPUT argument n and number of terms
-#RETURN approximation of e(n) for num_terms.
-def my_e(n,num_terms):
-    pass
-
 
 
 # problem 4
-#INPUT a string
-#RETURN a dictionary of all the proper substrings with counts of occurences
+# INPUT a string
+# RETURN a dictionary of all the proper substrings with counts of occurences
 def ss(x):
     pass
 
 
-
 # problem 5
-#INPUT old, new, list of lists
-#RETURN for each list in list, replace old with new
-def lst_replace(old,new,lst):
+# INPUT old, new, list of lists
+# RETURN for each list in list, replace old with new
+def lst_replace(old, new, lst):
+    nlst = []
+    # for num_lst in lst:
+    #     for i in num_lst:
+    #         if i == old:
+    #             nlst.append(new)
+    #         else:
+    #             nlst.append(i)
+    # Other tested (FAILED)
+    # for i in range(len(lst)):
+    #     if lst[i] == old:
+    #         old = new
+    #         nlst.append(old)
+    #     else:
+    #         nlst.append(old)
+    # return nlst
+
     pass
 
 
 # problem 6
-#INPUT list 
-#INPUT remove any list brackets objects are in the same order
+# INPUT list
+# INPUT remove any list brackets objects are in the same order
 def foo(lst):
     pass
 
@@ -52,19 +74,19 @@ if __name__ == "__main__":
 
     """uncomment if you'd like"""
 
-    # #problem 1
+    # # problem 1
     # for i in range(6):
     #     print(f"{i}! = {factorial(i)}")
 
-    # #problem 2
-    # p2 = [[[1,0,0,1],[0,1,1,0]],[[],[]],[[1,2,3,4],[5,4,3,2]]]
+    # # problem 2
+    # p2 = [[[1, 0, 0, 1], [0, 1, 1, 0]], [[], []], [[1, 2, 3, 4], [5, 4, 3, 2]]]
 
-    # for x,y in p2:
+    # for x, y in p2:
     #     print(f"{x,y} {gl(x,y)}")
 
-    # #problem 3
+    # # problem 3
     # print(math.exp(5))
-    # print(my_e(5,16))
+    # print(my_e(5, 16))
 
     # #problem 4
     # p4 = ["s","abc","abcabc","aa"]
@@ -72,12 +94,12 @@ if __name__ == "__main__":
     #     print(d)
     #     print(ss(d))
 
-    # #problem 5
-    # lst = [[1,2],[2,[2],1],[],[1,1,]]
-    # print(lst_replace(1,"dog", [[1,2],[2,[2],1],[],[1,1,]]))
-    # print(lst_replace(2,"dog", [[1,2],[2,[2],1],[],[1,1,]]))
-    # print(lst_replace([2],"dog", [[1,2],[2,[2],1],[],[1,1,]]))
-    # print(lst_replace(1,"dog", []))
+    # problem 5
+    lst = [[1, 2], [2, [2], 1], [], [1, 1, ]]
+    print(lst_replace(1, "dog", [[1, 2], [2, [2], 1], [], [1, 1, ]]))
+    print(lst_replace(2, "dog", [[1, 2], [2, [2], 1], [], [1, 1, ]]))
+    print(lst_replace([2], "dog", [[1, 2], [2, [2], 1], [], [1, 1, ]]))
+    print(lst_replace(1, "dog", []))
 
     # #problem 6
     # p6 = [[[1],2,[[3]]], [[1,[2,[3]],4,[[5]],6,[7,8]]],
