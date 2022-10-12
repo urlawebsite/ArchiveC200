a695ab46d45cc38d82c6a1b07263d06a1ef00bae
Parichit Sharma <parichitsharma08@gmail.com>
2022-10-12 13:05:55 -0400

    Pushing files for Midterm

--- Midterm/midterm.py
+++ Midterm/midterm.py
@@ -3,3 +3,25 @@
 # Write you code for midterm problems in this file.
 # Do not change the name of this file.
 ########
+
+# Your functions goes here
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	"""
+    If you want to do some of your own testing in this file, 
+    please put any print statements you want to try in 
+    this if statement.
+
+    Please comment the print statements before submitting your 
+    final version.
+	"""
+
+
