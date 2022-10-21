e1d1d83e7a0ace66cc28a8cc60d09c49139b25b4
uzrivera <uzrivera@iu.edu>
2022-10-21 14:03:39 -0400

    Added Lab6

---
+++ Laboratory/Lab6/lab6.py
@@ -0,0 +1,86 @@
+###########################################################
+# factorial
+###########################################################
+
+def factorial(n):
+    """
+    Recursive function to calculate the factorial of n
+
+    Input:
+        n (an integer)
+    Returns:
+        n! = n*(n-1)*...*2*1
+    """
+    pass
+
+def tail_factorial(n, a=1):
+    """
+    Tail-recursive function to calculate the factorial of n
+
+    Input:
+        n (an integer)
+    Returns:
+        n! = n*(n-1)*...*2*1
+    """
+    pass
+
+d = {}
+def memo_factorial(n):
+    """
+    Memoized function to calculate the factorial of n
+
+    Input:
+        n (an integer)
+    Returns:
+        n! = n*(n-1)*...*2*1
+    """
+    pass
+
+###########################################################
+# only_ints
+###########################################################
+
+def only_ints(xlist):
+    """
+    Recursive function to return a list with all non-ints taken
+    out of it.
+
+    Input:
+        xlist - a list of elements
+    Returns:
+        xlist, but with only the 'int'-type elements kept.
+
+    """
+    pass
+
+def tail_only_ints(xlist, a=[]):
+    """
+    Recursive function to return a list with all non-ints taken
+    out of it.
+
+    Input:
+        xlist - a list of elements
+    Returns:
+        xlist, but with only the 'int'-type elements kept.
+
+    """
+    pass
+
+d = {}
+def memo_only_ints(xlist):
+    """
+    Recursive function to return a list with all non-ints taken
+    out of it.
+
+    Input:
+        xlist - a list of elements
+    Returns:
+        xlist, but with only the 'int'-type elements kept.
+
+    """
+    pass
+
+if __name__ == "__main__":
+    # Write your own print statements here
+    # to briefly test your code
+    pass
