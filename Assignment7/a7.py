829ed4ac38d014a7a7bb45e5fc55061a719e34a5
uzrivera <uzrivera@iu.edu>
2022-11-05 11:58:29 -0400

    Finishing up assignment

--- Assignment7/a7.py
+++ Assignment7/a7.py
@@ -1,90 +1,133 @@
-## Write the name of your programming partner below
+# Write the name of your programming partner below
 # partner name: _________________
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 import os
 
 ###############
 # PROBLEM ONE
 ###############
 
-#choice
+# choice
 # You can choose this function to complete B()
+
+
 def C(n, k):
-    if k == 0 or k == m:
+    if k == 0 or k == n:
         return 1
     else:
         return C(n-1, k) + C(n-1, k-1)
 
-#Input: a number n
-#Return: The output of B as per HW instructions
+# Input: a number n
+# Return: The output of B as per HW instructions
+
+
 def B(n):
-    pass
-        
- 
+    if n == 0:
+        return 1
+    else:
+        k = 0
+        nlst = []
+        while k <= n-1:
+            nlst += [C(n+1, k)*B(k)]
+            k = k+1
+        return (sum(nlst)*-1)/(n+1)
 
-###############
-# PROBLEM TWO
-###############
+        ###############
+        # PROBLEM TWO
+        ###############
 
-#Input: a number n
-#Return: The output of a() as per HW instructions
+        # Input: a number n
+        # Return: The output of a() as per HW instructions
+
+
 def a(n):
-    pass
+    if n == 1:
+        return 3
+    elif n == 2:
+        return 5
+    else:
+        if n <= 0:
+            return 2
+        else:
+            return a(n-1)+a(n-2)+a(n-3)
 
-#Input: a number n
-#Return: The output using while loop
+# Input: a number n
+# Return: The output using while loop
+
+
 def aw(n):
     pass
 
-#Input: generator don't need to have any input. Instead, you can set the base case values and then
+# Input: generator don't need to have any input. Instead, you can set the base case values and then
 # use 'yield' keyword inside a loop
-#Return: The output via using generators
+# Return: The output via using generators
+
+
 def a_gen():
     pass
 
-#Input: a number n
-#Return: The output using tail recursion
+# Input: a number n
+# Return: The output using tail recursion
+
+
 def at(n, v0=2, v1=3, v2=5):
     pass
 
-#Input: a number x
-#Return: The output of bb() as per HW instructions
-def bb(x):
-    pass 
+# Input: a number x
+# Return: The output of bb() as per HW instructions
 
-#Input: a number x
-#Return: The output using while loop
+
+def bb(x):
+    pass
+
+# Input: a number x
+# Return: The output using while loop
+
+
 def bbw(x):
     pass
 
-#Input: generator don't need to have any input. Instead, you can set the base case values and then
+# Input: generator don't need to have any input. Instead, you can set the base case values and then
 # use 'yield' keyword inside a loop
-#Return: The output via using generators
+# Return: The output via using generators
+
+
 def bb_gen():
     pass
 
-#Input: a number n
-#Return: The output using tail recursion
-def bbt(x,v0=2,v1=-3):
+# Input: a number n
+# Return: The output using tail recursion
+
+
+def bbt(x, v0=2, v1=-3):
     pass
 
-#Input: numbers n, m ,p
-#Return: The output of F() as per HW instructions
-def F(n,m,p):
+# Input: numbers n, m ,p
+# Return: The output of F() as per HW instructions
+
+
+def F(n, m, p):
     pass
 
-#Input: a number n, m ,p
-#Return: The output using tail recursion
-def Ft(n,m,p,v = 100):
+# Input: a number n, m ,p
+# Return: The output using tail recursion
+
+
+def Ft(n, m, p, v=100):
     pass
 
-#Input: a number n, m, p
-#Return: The output using while loop
-def Fw(n,m,p):
-    pass 
+# Input: a number n, m, p
+# Return: The output using while loop
 
 
-def m(x,y):
+def Fw(n, m, p):
+    pass
+
+
+def m(x, y):
     if x <= 0 and y <= 0:
         return 3
     elif x <= 0:
@@ -92,43 +135,59 @@ def m(x,y):
     elif y <= 0:
         return 1
     else:
-        return m(x-1,y-1) + m(x-1,y-2)
+        return m(x-1, y-1) + m(x-1, y-2)
 
 # Input: numbers x and y
-# Return: The output using while loop 
-def mw(x,y):
+# Return: The output using while loop
+
+
+def mw(x, y):
     pass
+
 
 ###############
 # PROBLEM THREE
 ###############
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
 
-#INPUT takes path + filename (I am using a txt file)
-#RETURN returns list of tuples [(year,population),...], stick to the format of return values.
-def get_data(path,name):
-    pass
+# INPUT takes path + filename (I am using a txt file)
+# RETURN returns list of tuples [(year,population),...], stick to the format of return values.
+def get_data(path, name):
+    title = "Years + 1900    Population x10^6"
+    with open(name, "w") as path:
+        path.write(title + '\n')
+    data = [(0, 1650), (10, 1750), (20, 1860), (30, 2070), (40, 2300), (50, 2560),
+            (60, 3040), (70, 3710), (80, 4450), (90, 5280), (100, 6080), (110, 6870)]
+    for items in data:
+        with open(name, "a") as path:
+            path.write(str(items[0]) + " " + str(items[1]) + '\n')
+    return data
 
 # Input: a year
 # Return: the estimate of population for that year
+
+
 def pop(year):
-    pass
+    return 1436.53*(1.01395**year)
+
 
 # Input: data
 # Return: the average error as per equation 18.
-def error(data):
-    pass
 
+def error(data):
+    a = 0
+    for i in range(0, 12):
+        a += ((data[i][1])-pop(i*10))
+    return (1/(len(data)))*a
 
 ###############
 # PROBLEM four
 ###############
-#You cannot simply divide or use modulus
-#You must implement the algorithm as described
-#INPUT a number n 
-#RETURN True if dvisible by 11, False if not.
+# You cannot simply divide or use modulus
+# You must implement the algorithm as described
+# INPUT a number n
+# RETURN True if dvisible by 11, False if not.
+
+
 def div_11(n):
     pass
 
@@ -138,10 +197,10 @@ def div_11(n):
 ###############
 # INPUT list of numbers
 # RETURN list of nesting level and sum at that level
-def sl(lst,p=0):
-   pass
+def sl(lst, p=0):
+    pass
 
-#PROBLEM 6
+# PROBLEM 6
 # Uncomment the following code after finishing the population problem
 # Please remember to comment it back before submitting to the Autograder
 # We will mnaually run the code to see if the code correctly draws the trainagles.
@@ -159,8 +218,8 @@ def sl(lst,p=0):
 
 # pygame.display.set_caption('S-Triangle')
 
-#INPUT takes a location loc = (x,y) pair of points and width
-#RETURN 3 points of the equilateral triangle determined by loc and width
+# INPUT takes a location loc = (x,y) pair of points and width
+# RETURN 3 points of the equilateral triangle determined by loc and width
 # def triangle(loc,width):
 #     x,y = loc
 #     z = math.sqrt(width**2 - (width/2))
@@ -168,16 +227,16 @@ def sl(lst,p=0):
 
 # DISPLAYSURF.fill(BLACK)
 
-#Draws Triangle
-#(triangle(loc,w)) is a tuple of tuples...)
+# Draws Triangle
+# (triangle(loc,w)) is a tuple of tuples...)
 # def draw_triangle(loc, w):
 #     pygame.draw.polygon(DISPLAYSURF, (rn.randint(0,255),rn.randint(0,255),rn.randint(0,255)) , (triangle(loc,w)),1)
 
 
-#INPUT location and width
-#RETURN nothing -- follows algorithm
-#Draw the three smaller triangles as described
-#in the text
+# INPUT location and width
+# RETURN nothing -- follows algorithm
+# Draw the three smaller triangles as described
+# in the text
 # def s(loc,width):
 #     if width > 1:
 #         x,y = loc
@@ -193,16 +252,16 @@ if __name__ == "__main__":
     your code outside of the `test_a7.py`. Feel free to add print statements. 
     # """
 
-    #PROBLEM 1
-    
-    # for i in range(6):
-    #     print("B({0}) = {1}".format(i,B(i)))
+    # PROBLEM 1
 
-    #PROBLEM 2
+    # for i in range(6):
+    #     print("B({0}) = {1}".format(i, B(i)))
+
+    # PROBLEM 2
     # for i,j in zip(range(10),a_gen()):
-    #     print(f"a({i}) {a(i)} {aw(i)} {at(i)} {j} \n",end="")   
+    #     print(f"a({i}) {a(i)} {aw(i)} {at(i)} {j} \n",end="")
     # for i,j in zip(range(10),bb_gen()):
-    #     print(f"b({i}) {bb(i)} {bbw(i)} {bbt(i)} {j}")    
+    #     print(f"b({i}) {bb(i)} {bbw(i)} {bbt(i)} {j}")
     # for i in range(6):
     #     for j in range(6):
     #         for k in range(6):
@@ -211,43 +270,43 @@ if __name__ == "__main__":
     #     for j in range(10):
     #         print(f"{i,j} {m(i,j)} {mw(i,j)} ")
 
-    #Problem 3
-    # data = get_data(".", "pop.txt")
-    # print(f"data from file:",data)
-    # print(f"abs(3040 - {pop(60)}) = {abs(3040 - pop(60))}")
-    # ave_error = round(error(data),2)
-    # print(f"Average error: {ave_error}")
+    # Problem 3
+    data = get_data(".", "pop.txt")
+    print(f"data from file:", data)
+    print(f"abs(3040 - {pop(60)}) = {abs(3040 - pop(60))}")
+    ave_error = round(error(data), 2)
+    print(f"Average error: {ave_error}")
 
     # #uncomment to see plot
     # Please comment it back after you are done
     # completing the functions, otherwise the Autograder will
     # return with error.
-    
-    # t = np.arange(0.0, 120.0)
-    # fig,ax = plt.subplots()
 
-    # ax.plot(t, pop(t),'g')
-    # for y,p in data:
-    #     ax.plot(y,p,'ro--')
+    t = np.arange(0.0, 120.0)
+    fig, ax = plt.subplots()
 
-    # ax.set(xlabel ="Time (Year + 1900)", ylabel=r"Pop size $\times 10^6$",
-    # title = "Population Model Average Error = {0}".format(ave_error))
-    # ax.grid()
-    # plt.show()
+    ax.plot(t, pop(t), 'g')
+    for y, p in data:
+        ax.plot(y, p, 'ro--')
 
-    #problem 4
+    ax.set(xlabel="Time (Year + 1900)", ylabel=r"Pop size $\times 10^6$",
+           title="Population Model Average Error = {0}".format(ave_error))
+    ax.grid()
+    plt.show()
+
+    # problem 4
     # nlst = [587657752,11,22,2728,31415,1358016]
     # for n in nlst:
     #     print(div_11(n), n / 11)
 
-    #problem 5
+    # problem 5
     # prob5 = [[1,4,[3,[100]],3,2,[1,[101,1000],5],1,[7,9]],
     #          [1,2,3,4,[10,20,30,40,[100,200,300,400]]],
     #          [[[[100,200,300,400]]],[[10,20,30,40]],1,2,3,4]]
     # for lst in prob5:
     #     print(sl(lst))
 
-    #Problem 6
+    # Problem 6
     # s((240,0),440)
 
     # while True:
