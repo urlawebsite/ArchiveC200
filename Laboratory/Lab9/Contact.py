f6d919ccdad65700a66606b7cd120e5edfb9763b
uzrivera <uzrivera@iu.edu>
2022-11-11 15:30:21 -0500

    Finish lab9

--- Laboratory/Lab9/Contact.py
+++ Laboratory/Lab9/Contact.py
@@ -13,28 +13,52 @@ class Contact:
             - Email:        email
             - BirthDate:    birthdate
         """
-        
+        # Cannot print last name (out of range error)
+        # Pervious attempts:
+        # self.first = nameList[0]+" "+nameList[1]+" " but this one function won't work+nameList[2]
+        # self.first = nameList[0]
+        # self.middle = nameList[1]
+        # # self.last = nameList[1:]
+        # self.mobile = phoneNumber
+        # self.email = email
+        # self.birthdate = birthday
+        # Correct answer:
+        self.first = nameList[0]
+        if len(nameList) == 3:
+            self.middle = nameList[1]
+            self.last = nameList[2]
+        else:
+            self.middle = ""
+            self.last = nameList[1]
+
+        self.mobile = phoneNumber
+        self.email = email
+        self.birthdate = birthday
 
     def getName(self):
-        """
-        Return the name as a string like this "first middle last"
-        The middle name might not exist
-        """
-        
+
+        # return self.first + " " + self.middle
+        if self.middle:
+            return self.first+" " + self.middle+" " + self.last
+        else:
+            return self.first + " " + self.last
 
     def __str__(self):
         """
         Returns a string representing the contact:
         > Contact: Josh Baker - 3178675309
         """
-        
-    
+        # This is my attempt: and it kind of works
+        # return "Contact: " + self.getName() + " - " + "("+str(self.mobile)+")"
+        # Correct answer:
+        return "Contact:{} -({})".format(self.getName(), self.mobile)
+
     def __repr__(self):
         """
         Provided: 
         - Feel free to read more about it here: https://stackoverflow.com/questions/1436703/difference-between-str-and-repr
         """
-        return self.__str__() # This is provided. Helps with something printing a list of contact items. 
+        return self.__str__()  # This is provided. Helps with something printing a list of contact items.
 
     def call(self):
         """
@@ -42,31 +66,29 @@ class Contact:
         the user should be prompted to input a message and then it should
         be printed
         """
-        
-    
-
+        # print("Dialing...")
+        # print("No answer")
+        # print("Leave a message:")
+        # self.sendText(input())
+        # Correct answer:
+        print("Dialing...", self.first, "...")
 
     def sendBirthdayText(self):
         """
         There should be a premade birthday text that uses the name 
         and then prints it out when this is called
         """
-        
-
+        self.sendText("Happy Birthday " + self.first + "!")
 
     def sendText(self, message):
         """
         Given a message a text to the person
         """
-        
-    
+        print("To: " + self.first+":")
+        print("\t" + message)
 
     def updateNumber(self, phoneNumber):
         """
         Take in a new phone number and replace the user's current phone number
         """
-        
-
-
-
-    
+        self.mobile = phoneNumber
--- Laboratory/Lab9/PhoneBook.py
+++ Laboratory/Lab9/PhoneBook.py
@@ -1,5 +1,6 @@
 from Contact import Contact
 
+
 class PhoneBook:
 
     def __init__(self):
@@ -9,8 +10,10 @@ class PhoneBook:
 
         Instance variables provided for this constructor.
         """
+        # My answer works
+        self.contactsLst = []
+        self.count = 0
 
-    
     def addContact(self, c):
         """
         Given a contact, determine if you are given a dictionary or an instance of 
@@ -25,25 +28,50 @@ class PhoneBook:
 
         NOTE: Why do we have to manually update the counter?
         """
-    
+        # if type(c) == dict:
+        #     self.contacts.append(
+        #         Contact(c['name'], c['number'], c['email'], c['birthday']))
+        # elif type(c) == Contact:
+        #     self.contacts.append(c)
+        if isinstance(c, Contact):
+            self.contactsLst.append(c)
+        elif isinstance(c, dict):
+            name = c['name']
+            number = c['number']
+            email = c['email']
+            birthday = c['birthday']
+            newContact = Contact(name, number, email, birthday)
+            self.contactsLst.append(newContact)
+        self.count += 1
+
     def getContactCount(self):
         """
         Returns the number of contacts stored in the 
         """
-    
+        # Ans works
+        return self.count
+
     def findContact(self, lName):
         """
         Given a last name, find the contact(s) and return the contact information. 
 
         Will be a list. 
         """
-
-
+        # My answer works
+        self.contactInfo = []
+        for i in self.contactsLst:
+            if i.last == lName:
+                self.contactInfo.append(i)
+        return self.contactInfo
 
     def groupChat(self, message):
         """
         Send a message to every contact in the phonebook
         """
+        # Ans dont work
+        # self.message = message
+        for i in self.contactsLst:
+            i.sendMessage(message)
 
     def __str__(self):
         """
@@ -54,4 +82,6 @@ class PhoneBook:
 
         Where # is the number of contacts in the phonebook. 
         """
-
+        # Kind of works
+        # return "Phone Book: " + str(self.getContactCount())
+        return "Phone Book: {}".format(self.getContactCount())
